9c8d7107c68a78fb25493419b8fb1d7c1f6692a3
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2024-09-10 14:19:31 -0600

    Se implementa reporte de productos

--- public/index.php
+++ public/index.php
@@ -1,13 +1,15 @@
-<?php 
+<?php
 require_once __DIR__ . '/../includes/app.php';
 
 
+use Controllers\ReporteController;
 use MVC\Router;
 use Controllers\AppController;
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
-$router->get('/', [AppController::class,'index']);
+$router->get('/', [AppController::class, 'index']);
+$router->get('/pdf', [ReporteController::class, 'pdf']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
